a58cb4e3fc7ff53b7e99c15f0a4d2106a0bc276f
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-01-02 10:10:32 +0000

    Remove the config file to work with multiple platforms

--- activemq-wrapper.py
+++ activemq-wrapper.py
@@ -5,19 +5,35 @@ import xml.etree.ElementTree as ET
 import time
 import sys
 import urllib2
+import socket
+import platform
+
 
 SCRIPT_FOLDER = os.path.dirname(os.path.realpath(__file__))
 CACHE_FOLDER = os.path.join(SCRIPT_FOLDER, ".cache")
 CONF_FOLDER = os.path.join(SCRIPT_FOLDER, "conf")
 
+# ~~~~ The list of ports:  (see activemq.xml)
+# Openwire: 21616
+# AMQP:     25672
+# STOMP:    21613
+# MQTT:     21883
+# WS:       21614
+#
+# WebConsole: 28161
+# JMX port: 20011  jmxDomainName=activemq.test.instance -> TEST.BROKER
+
 
 def main(command_word):
-    keys = parse_properties_file()
     if not os.path.exists(CACHE_FOLDER):
         os.mkdir(CACHE_FOLDER)
 
-    broker_version = keys["BROKER_VERSION"]
-    destination_url = keys["DESTINATION_URL"]
+    broker_version = "5.11.1"
+    if "Windows" in platform.system():
+        destination_url = "http://archive.apache.org/dist/activemq/5.11.1/apache-activemq-5.11.1-bin.zip"
+    else:
+        destination_url = "http://archive.apache.org/dist/activemq/5.11.1/apache-activemq-5.11.1-bin.tar.gz"
+
     print "Requiring ActiveMq version: " + broker_version
     activemq_home = os.path.join(CACHE_FOLDER, broker_version)
     activemq_bin = os.path.join(activemq_home, "bin/activemq")
@@ -27,7 +43,7 @@ def main(command_word):
         print "artifact: " + downloaded_artifact
         print "Version not found in local cache. Downloading from: " + destination_url
 
-        download_file(destination_url, downloaded_artifact)
+        download_and_show_progress(destination_url, downloaded_artifact)
         subprocess.call(["tar", "-xvf", downloaded_artifact, "-C", CACHE_FOLDER, "--transform", "s/apache-activemq-//"])
 
     os.chmod(activemq_bin, 0x755)
@@ -48,25 +64,27 @@ def main(command_word):
         wait_until_port_is_open(admin_port, 5)
 
 
-def parse_properties_file():
-    separator = "="
-    keys = {}
+def download_and_show_progress(url, file_name):
+    u = urllib2.urlopen(url)
+    f = open(file_name, 'wb')
+    meta = u.info()
+    file_size = int(meta.getheaders("Content-Length")[0])
+    print "Downloading: %s Bytes: %s" % (file_name, file_size)
 
-    with open(os.path.join(SCRIPT_FOLDER, 'activemq-wrapper.properties')) as f:
-        for line in f:
-            if separator in line:
-                name, value = line.split(separator, 1)
-                value = value.replace("${SCRIPT_FOLDER}", SCRIPT_FOLDER)
-                if not name.startswith("#"):
-                    keys[name.strip()] = value.strip()
+    file_size_dl = 0
+    block_sz = 8192
+    while True:
+        buffer = u.read(block_sz)
+        if not buffer:
+            break
 
-    return keys
+        file_size_dl += len(buffer)
+        f.write(buffer)
+        status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+        status = status + chr(8) * (len(status) + 1)
+        print status,
 
-
-def download_file(url, file_name):
-    output = urllib2.urlopen(url)
-    with open(file_name, 'wb') as f:
-        f.write(output.read())
+    f.close()
 
 
 def parse_activemq_xml(conf_file):
@@ -95,8 +113,9 @@ def wait_until_port_is_open(port, delay):
     n = 0
     while n < 5:
         print "Is application listening on port " + str(port) + "? "
-        returncode = subprocess.call("lsof -P -i TCP:" + str(port) + " > /dev/null", shell=True)
-        if returncode == 0:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = sock.connect_ex(('127.0.0.1', int(port)))
+        if result == 0:
             print "Yes"
             return
         print "No. Retrying in " + str(delay) + " seconds"
